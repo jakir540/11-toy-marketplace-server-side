3f8056e3a5b8c5a409dfeb210a7085fada39e7c6
md jakir hossain <jakirhossainmd540@gmail.com>
2023-05-18 22:39:13 +0600

    get the specific toy and display details toy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app= express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -34,6 +34,15 @@ async function run() {
     app.get('/toys',async(req,res)=>{
       const result = await toysCollection.find().toArray()
       res.send(result);
+    })
+
+    //get the specific card by id 
+
+    app.get('/viewDetailsCard/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result= await toysCollection.find(query).toArray();
+      res.send(result)
     })
 
 
