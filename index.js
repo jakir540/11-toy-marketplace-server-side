05539e61d408c485d8212db3c8300e3823a751a7
jakir540 <jakirhossainmd540@gamil.com>
2025-01-21 22:04:15 +0600

    add blog functinlaity route

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT || 5000
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
+// middleware
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
-
+console.log(process.env.USER_SEC);
 
 const uri = `mongodb+srv://${process.env.USER_SEC}:${process.env.USER_PASS}@cluster0.wkiarbk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,138 +28,127 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
+    const toysCollection = client.db("carToys").collection("toys");
+    const bookingCollection = client.db("carToys").collection("booking");
+    const blogCollection = client.db("carToys").collection("blog");
 
-    const toysCollection = client.db('carToys').collection('toys')
-    const bookingCollection = client.db('carToys').collection('booking')
-
-
-    const indexKey = { category: 1 }
+    const indexKey = { category: 1 };
     const indexOption = { name: "searceByName" };
 
-    const result = await bookingCollection.createIndex(indexKey, indexOption)
+    const result = await bookingCollection.createIndex(indexKey, indexOption);
 
-
-
-    app.get('/toySearceByName/:name', async (req, res) => {
+    app.get("/toySearceByName/:name", async (req, res) => {
       const searceText = req.params.name;
-      const result = await bookingCollection.find({ category: { $regex: searceText, $options: "i" } }).toArray()
-      res.send(result)
-    })
-
-
-
-
-
-    app.get('/toys', async (req, res) => {
-      const cursor = toysCollection.find()
-      const result = await cursor.toArray()
+      const result = await bookingCollection
+        .find({ category: { $regex: searceText, $options: "i" } })
+        .toArray();
       res.send(result);
-    })
+    });
 
-    //get the specific card by id 
+    app.get("/toys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/viewDetailsCard/:id', async (req, res) => {
+    //get the specific card by id
+
+    app.get("/viewDetailsCard/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
 
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.findOne(query);
-     if (result) {
-      console.log("data found");
-     }else{
-      console.log("not found");
-     }
-      res.send(result)
-    })
+      if (result) {
+        console.log("data found");
+      } else {
+        console.log("not found");
+      }
+      res.send(result);
+    });
 
-
-
-
-    // Searcing Toys by category 
-    app.get('/searceToyByName/:text', async (req, res) => {
+    // Searcing Toys by category
+    app.get("/searceToyByName/:text", async (req, res) => {
       const searceText = req.params.text;
       console.log(searceText);
-      if (searceText == "bus" || searceText == "Trucks" || searceText == "Bikes") {
-        const result = await toysCollection.find({ subcategory: searceText }).toArray();
-        return res.send(result)
-
+      if (
+        searceText == "bus" ||
+        searceText == "Trucks" ||
+        searceText == "Bikes"
+      ) {
+        const result = await toysCollection
+          .find({ subcategory: searceText })
+          .toArray();
+        return res.send(result);
       }
       const result = await toysCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    // Add specific Toy 
-    app.post('/addToy', async (req, res) => {
-
+    // Add specific Toy
+    app.post("/addToy", async (req, res) => {
       const body = req.body;
       if (!body) {
-        return res.status(404).send({ message: "data not found" })
+        return res.status(404).send({ message: "data not found" });
       }
       const result = await bookingCollection.insertOne(body);
       res.send(result);
+    });
 
-    })
+    // get all booking Toys
 
-    // get all booking Toys 
-
-    app.get('/getToy', async (req, res) => {
-      const data = toysCollection.find().limit(20)
+    app.get("/getToy", async (req, res) => {
+      const data = toysCollection.find().limit(20);
       const result = await data.toArray();
       res.send(result);
-    })
+    });
 
-
-
-    app.get('/myToys/:name', async (req, res) => {
+    app.get("/myToys/:name", async (req, res) => {
       const searceText = req.params.name;
       // console.log(searceText);
-      const cursor = bookingCollection.find({ name: searceText })
+      const cursor = bookingCollection.find({ name: searceText });
       const result = await cursor.toArray();
       res.send(result);
+    });
 
-    })
-
-
-
-    // my toys sorting 
-    app.get('/myToysSorting', async (req, res) => {
+    // my toys sorting
+    app.get("/myToysSorting", async (req, res) => {
       const sort = req.query.sort;
       const options = {
         sort: {
-          price: sort == 'asc' ? 1 : -1
-        }
+          price: sort == "asc" ? 1 : -1,
+        },
       };
 
       // const cursor = bookingCollection.find(query,options).collation({ locale: "en_US", numericOrdering: true })
       const cursor = bookingCollection.find(options);
       const result = await cursor.toArray();
       res.send(result);
-
-    })
-
+    });
 
     //Delete specific toy
 
-    app.delete('/bookingsToys/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
+    app.delete("/bookingsToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     //update toy
 
-    app.put('/updateToy/:id', async (req, res) => {
+    app.put("/updateToy/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) }
+      console.log("154", id);
+      const filter = { _id: new ObjectId(id) };
       const updateToy = req.body;
       console.log(updateToy);
 
@@ -160,19 +157,30 @@ async function run() {
         $set: {
           price: updateToy.price,
           quantity: updateToy.quantity,
-          description: updateToy.description
-        }
+          description: updateToy.description,
+        },
+      };
+
+      const result = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // Add blog Toy
+    app.post("/blog", async (req, res) => {
+      const body = req.body;
+      console.log({ body });
+      if (!body) {
+        return res.status(404).send({ message: "blog not found" });
       }
-
-      const result = await bookingCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-
-    })
-
+      const result = await blogCollection.insertOne(body);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -180,12 +188,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send("volunteer server is running ontime ")
-})
+app.get("/", (req, res) => {
+  res.send("volunteer server is running ontime ");
+});
 
 app.listen(port, () => {
   console.log(`volunteer server is port Number ${port}`);
-})
+});
